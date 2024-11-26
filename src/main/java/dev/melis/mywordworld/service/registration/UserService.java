c4b0b19de06432245f46e8a9fc67c201c133745b
Melis Olmez <olmezmelis204@gmail.com>
2024-11-26 21:18:25 +0300

    update user service

--- src/main/java/dev/melis/mywordworld/service/registration/UserService.java
+++ src/main/java/dev/melis/mywordworld/service/registration/UserService.java
@@ -2,7 +2,9 @@ package dev.melis.mywordworld.service.registration;
 
 import dev.melis.mywordworld.support.result.CreationResult;
 import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface UserService {
 
     CreationResult register(@Valid UserServiceRequest userRequest);
--- src/main/java/dev/melis/mywordworld/service/registration/UserServiceImpl.java
+++ src/main/java/dev/melis/mywordworld/service/registration/UserServiceImpl.java
@@ -4,16 +4,20 @@ import java.util.Optional;
 
 import dev.melis.mywordworld.model.User;
 import dev.melis.mywordworld.repository.UserRepository;
+import dev.melis.mywordworld.support.passwordencoder.PasswordEncoderAdaptor;
 import dev.melis.mywordworld.support.result.CreationResult;
 import dev.melis.mywordworld.support.result.OperationFailureReason;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final PasswordEncoderAdaptor passwordEncoderAdaptor;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoderAdaptor passwordEncoderAdaptor) {
         this.userRepository = userRepository;
+        this.passwordEncoderAdaptor = passwordEncoderAdaptor;
     }
 
     @Override
@@ -25,7 +29,8 @@ public class UserServiceImpl implements UserService {
 
         var user = new User();
         user.setEmail(userRequest.getEmail());
-        user.setPassword(userRequest.getPassword());
+        String hashedPassword = passwordEncoderAdaptor.encode(userRequest.getPassword());
+        user.setPassword(hashedPassword);
         user.setName(userRequest.getName());
         user.setSurname(userRequest.getSurname());
         userRepository.save(user);
