265b553bddeae7c29970491b08336e63320271aa
Melis Olmez <olmezmelis204@gmail.com>
2024-11-26 15:11:32 +0300

    Initial project setup with .gitignore and core files

---
+++ src/main/java/dev/melis/mywordworld/MywordworldApplication.java
@@ -0,0 +1,12 @@
+package dev.melis.mywordworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywordworldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MywordworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/melis/mywordworld/MywordworldApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.melis.mywordworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywordworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
