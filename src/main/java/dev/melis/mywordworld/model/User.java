86aa7c96806b618df45fe3538b5206b9f3e18298
Melis Olmez <olmezmelis204@gmail.com>
2024-11-26 21:15:04 +0300

    update user class with adding userdetails class for jwt

--- src/main/java/dev/melis/mywordworld/model/User.java
+++ src/main/java/dev/melis/mywordworld/model/User.java
@@ -1,16 +1,22 @@
 package dev.melis.mywordworld.model;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -19,4 +25,37 @@ public class User {
     private String surname;
     private String email;
     private String password;
+
+    public User(){
+        super();
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
