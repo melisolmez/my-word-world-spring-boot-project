a0270527dc92e6d7ead5a0f3f7ab9a0e98e2e682
Melis Olmez <olmezmelis204@gmail.com>
2024-11-30 20:52:15 +0300

    add bean for WebClient

--- src/main/java/dev/melis/mywordworld/config/ApplicationConfig.java
+++ src/main/java/dev/melis/mywordworld/config/ApplicationConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig {
@@ -42,5 +44,11 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.build();
+
     }
 }
