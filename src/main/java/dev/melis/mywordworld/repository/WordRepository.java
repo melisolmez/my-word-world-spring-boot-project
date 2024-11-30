0a15ce642fd216e6c05e554f1f23aea5a45db581
Melis Olmez <olmezmelis204@gmail.com>
2024-11-30 20:50:50 +0300

    add word repository

---
+++ src/main/java/dev/melis/mywordworld/repository/WordRepository.java
@@ -0,0 +1,9 @@
+package dev.melis.mywordworld.repository;
+
+import dev.melis.mywordworld.model.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+}
