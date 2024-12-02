50e7622884404ecab725c50f6a76af67f37643a5
Melis Olmez <olmezmelis204@gmail.com>
2024-12-02 23:54:50 +0300

    update wordnik api service

--- src/main/java/dev/melis/mywordworld/service/wordsapi/WordnikService.java
+++ src/main/java/dev/melis/mywordworld/service/wordsapi/WordnikService.java
@@ -1,9 +1,11 @@
 package dev.melis.mywordworld.service.wordsapi;
 
+import dev.melis.mywordworld.config.UserSession;
+
 public interface WordnikService {
 
     String getWordDefinition(String word);
     String getWordExample(String word);
     String getWordPronunciationAudio(String word);
-    void getAndSaveWordInformation(String word);
+    WordnikServiceDTO getAndSaveWordInformation(String word, UserSession userSession);
 }
