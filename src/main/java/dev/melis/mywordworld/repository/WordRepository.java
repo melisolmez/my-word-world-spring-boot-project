204b91a985e0fa3691196c227756ab3317557b43
Melis Olmez <olmezmelis204@gmail.com>
2024-12-03 22:25:04 +0300

    add method in the word repository

--- src/main/java/dev/melis/mywordworld/repository/WordRepository.java
+++ src/main/java/dev/melis/mywordworld/repository/WordRepository.java
@@ -1,9 +1,16 @@
 package dev.melis.mywordworld.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import dev.melis.mywordworld.model.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
+    Optional<Word> findByWordAndUserId(String word,Long userId);
+    List<Word> findByUserId(Long userId);
+
+    Optional<Word> findByWord(String word);
 }
